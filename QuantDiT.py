9c94be09f805e5cbee92b7f9966430d1752d1eff
Zhenyuan Dong <77495162+DZY122@users.noreply.github.com>
2024-11-21 01:07:22 -0500

    Update QuantDiT.py

--- QuantDiT.py
+++ QuantDiT.py
@@ -40,7 +40,7 @@ def main(args):
     torch.manual_seed(args.seed)
     #torch.set_grad_enabled(False)
     torch.set_grad_enabled(True)
-    device = "cuda:2" if torch.cuda.is_available() else "cpu"
+    device = "cuda:0" if torch.cuda.is_available() else "cpu"
 
     if args.ckpt is None:
         assert args.model == "DiT-XL/2", "Only DiT-XL/2 models are available for auto-download."
